265510ad5465261a2bd5582b1c6f88c4249dbddb
Razib2019 <hafizurrahmanrazib@gmail.com>
2022-08-31 09:02:53 +0600

    summary.js done

---
+++ summary.js
@@ -0,0 +1,15 @@
+//Must Have to Know
+
+// 1.Optional Chaining
+// student.marks?.math
+
+//2. map, filter, find
+
+// 3. class
+
+// 4.prototypical Inheritance (Interview Questions-Most Important)
+
+
+// Option Thing Have to know
+// 1. forEach, reduce(advance)
+// 2. Inheritance, super
